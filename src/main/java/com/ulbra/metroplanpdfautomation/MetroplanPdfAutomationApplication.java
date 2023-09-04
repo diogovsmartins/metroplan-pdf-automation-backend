ae1bbc2543a5d01a5cdaf7505948be27f0888955
diogovsmartins <diogodmartins@rede.ulbra.br>
2023-09-04 20:17:49 -0300

    Initial commit

---
+++ src/main/java/com/ulbra/metroplanpdfautomation/MetroplanPdfAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.ulbra.metroplanpdfautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroplanPdfAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroplanPdfAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ulbra/metroplanpdfautomation/MetroplanPdfAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulbra.metroplanpdfautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroplanPdfAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
